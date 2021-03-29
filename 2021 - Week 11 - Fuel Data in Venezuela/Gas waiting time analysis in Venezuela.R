b6211cd3e9b90530e607970d22769504221d1865
vivinoguera <lnoguera171@gmail.com>
2021-03-29 12:37:14 -0400

    gas visz ready

--- 2021 - Week 11 - Fuel Data in Venezuela/Gas waiting time analysis in Venezuela.R
+++ 2021 - Week 11 - Fuel Data in Venezuela/Gas waiting time analysis in Venezuela.R
@@ -29,33 +29,41 @@ theme_c4v <- theme(
   
   plot.background = element_rect(fill = background),
   panel.background = element_rect(fill = background),
-  plot.title = element_text(size = 20, colour = text_color, hjust = 0.5, margin = margin(10, 0, 10,0)),
-  
+  plot.title = element_text(size = 20, colour = text_color, hjust = 0.5, margin = margin(10, 0, 10,0), face = "bold"),
   
   panel.border = element_blank(),
   
   strip.background = element_blank(),
-  strip.text = element_text(family = font_type, size = 16, color = text_color),
+  strip.text = element_text(family = font_type, size = 16, color = "black"),
   
   legend.background = element_blank(),
   
-  axis.text = element_text(family = font_type ,size = 8, colour = text_color),
-  axis.title.y  = element_text(family = font_type ,size = 12, colour = text_color),
+  axis.text = element_text(family = font_type ,size = 14, colour = text_color),
+  axis.title.y  = element_text(family = font_type ,size = 14, colour = text_color),
   
-  
+  plot.margin = margin(0,0, 0, 0.5, unit = 'cm')
+
 )
 
 
-weekly_wait_time %>% 
-  ggplot(aes(month, avg_wait_time, color = state)) +
-  geom_line(show.legend = F) +
+wait_time_gas <- weekly_wait_time %>% 
+  ggplot(aes(month, avg_wait_time)) +
+  geom_area(show.legend = F, alpha = 0.7) +
+  geom_line(color = text_color, size = 0.7) +
   facet_wrap(~state, 4) +
   geom_text(aes(label = round(avg_wait_time, 1)), vjust =-0.3, show.legend = F) +
-  scale_colour_manual(values = c("#2b3051", "#2b3051", "#2b3051", "#2b3051")) +
   scale_y_continuous(breaks = seq(0,30,10)) +
+  scale_x_date(date_labels = "%b %y") +
   theme_c4v +
   labs(y = "Tiempo de Espera (Horas)",
        x = NULL,
-       title = "Promedio de Horas Mensual para Poner Gasolina en Venezuela")
+       title = "Promedio de Horas Mensual para Reponer Gasolina en Venezuela")
 
+wait_time_gas
+
+ggsave(wait_time_gas,
+       filename = "Tiempo de Espera Gasolina.png",
+       width = 10,
+       height = 14,
+       device = "png")
 
